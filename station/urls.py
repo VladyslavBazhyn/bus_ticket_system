596184e2f72319dabf090d5a7135a47b3ac92aa1
Vlad <mr.darmstadtium@gmail.com>
2024-05-20 21:04:03 +0200

    learning session 20.05.2024

--- station/urls.py
+++ station/urls.py
@@ -1,10 +1,35 @@
-from django.urls import path
-from station.views import bus_list, bus_detail
+from django.urls import path, include
+from station.views import BusViewSet
+from rest_framework import routers
 
 app_name = "station"
 
+router = routers.DefaultRouter()
+
+router.register("buses", BusViewSet)
+
 urlpatterns = [
-    path("buses/", bus_list, name="bus_list"),
-    path("buses/<int:pk>/", bus_detail, name="bus_detail")
+    path("", include(router.urls))
 ]
+
+# bus_list = BusViewSet.as_view(
+#     actions={
+#         "get": "list",
+#         "post": "create"
+#     }
+# )
+#
+# bus_detail = BusViewSet.as_view(
+#     actions={
+#         "get": "retrieve",
+#         "put": "update",
+#         "patch": "partial_update",
+#         "delete": "destroy"
+#     }
+# )
+#
+# urlpatterns = [
+#     path("buses/", bus_list, name="bus_list"),
+#     path("buses/<int:pk>/", bus_detail, name="bus_detail")
+# ]
 
--- station/views.py
+++ station/views.py
@@ -1,39 +1,9 @@
-from django.http import HttpRequest, JsonResponse
-from rest_framework.decorators import api_view
-from rest_framework.generics import get_object_or_404
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import viewsets, mixins
 
 from station.models import Buss
 from station.serializers import BussSerializer
 
 
-@api_view(["GET", "POST"])
-def bus_list(request) -> Response:
-    if request.method == "GET":
-        buses = Buss.objects.all()
-        serializer = BussSerializer(buses, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    else:
-        serializer = BussSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(["GET", "PUT", "DELETE"])
-def bus_detail(request, pk) -> Response:
-    bus = get_object_or_404(Buss, pk=pk)
-    if request.method == "GET":
-        serializer = BussSerializer(bus)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == "PUT":
-        serializer = BussSerializer(bus, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    else:
-        bus.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class BusViewSet(viewsets.ModelViewSet):
+    queryset = Buss.objects.all()
+    serializer_class = BussSerializer
