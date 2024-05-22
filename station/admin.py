7852da1ac94c9b960fcfdf0853e3f22e975dc4b5
Vlad <mr.darmstadtium@gmail.com>
2024-05-22 20:38:04 +0200

    nested serializers next

--- station/admin.py
+++ station/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
-from station.models import Buss
+from station.models import Buss, Ticket, Trip, Facility, Order
+
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInline, )
+
 
 admin.site.register(Buss)
+
+admin.site.register(Ticket)
+
+admin.site.register(Trip)
+
+admin.site.register(Facility)
+
+admin.site.register(Order, OrderAdmin)
--- station/serializers.py
+++ station/serializers.py
@@ -2,16 +2,26 @@ from rest_framework import serializers
 from station.models import Buss
 
 
-class BussSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    info = serializers.CharField(required=False, max_length=255)
-    num_seats = serializers.IntegerField(required=True)
-
-    def create(self, validated_data):
-        return Buss.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.info = validated_data.get("info", instance.info)
-        instance.num_seats = validated_data.get("num_seats", instance.num_seats)
-        instance.save()
-        return instance
+class BussSerializer(serializers.ModelSerializer):
+    # is_small = serializers.ReadOnlyField() # if fields = "__all__"
+    class Meta:
+        model = Buss
+        fields = [
+            "id",
+            "info",
+            "num_seats",
+            "is_small"
+        ]
+        read_only_fields = ["id"]
+    # id = serializers.IntegerField(read_only=True)
+    # info = serializers.CharField(required=False, max_length=255)
+    # num_seats = serializers.IntegerField(required=True)
+    #
+    # def create(self, validated_data):
+    #     return Buss.objects.create(**validated_data)
+    #
+    # def update(self, instance, validated_data):
+    #     instance.info = validated_data.get("info", instance.info)
+    #     instance.num_seats = validated_data.get("num_seats", instance.num_seats)
+    #     instance.save()
+    #     return instance
